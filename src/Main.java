98a62ab332bcd4f169bff97e496b77c6c77174eb
VictorHugo999 <victor.hugosilva999@gmail.com>
2024-06-08 21:02:24 -0300

    meu secundo commit, implementando um código simples

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,13 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Teste, primerio commit");
+        String nome;
+        String sobrenome;
+
+        nome = "Victor";
+        sobrenome = "Silva";
+
+        String nomeCompleto = nome.concat(" ").concat(sobrenome);
+
+        System.out.println("Olá, meu nome é " + nomeCompleto);
     }
 }
